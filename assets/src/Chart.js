d2dcd745ba3f4f600efd22a7000e5ee74efa68e6
Faye <faye@cht.com.tw>
2016-07-28 17:40:17 +0800

    modify chart css

--- assets/src/Chart.js
+++ assets/src/Chart.js
@@ -1,6 +1,8 @@
 import React, {PropTypes, Component} from 'react';
 import moment from 'moment';
 import { BarChart, d3 } from 'react-d3-components';
+import {ResponsiveBarChart, ResponsiveAreaChart, ResponsiveLineChart} from './ResponsiveChart';
+import Info from './Info';
 
 export default class Chart extends Component {
   static propTypes = {
@@ -21,38 +23,67 @@ export default class Chart extends Component {
       let chartData = [{label:'traffic', values:[]}]
       chartData[0].values = data.message.map(function(obj) {
         //moment.duration(obj.duration.value, "seconds").humanize()
+        let departureTime = moment(obj.time, "YYYYMMDDHHmm").format("HH:mm");
         if(!obj.hasOwnProperty('duration_in_traffic'))
-          return {x:obj.time, y:obj.duration.value}
+          return {x:departureTime, y:obj.duration.value}
         else
-          return {x:obj.time, y:obj.duration_in_traffic.value}
+          return {x:departureTime, y:obj.duration_in_traffic.value}
       });
-      //console.log(chartData);
+
+      let displayData = [];
+      displayData = data.message.map(function(obj) {
+        let duration = 0; // s
+        let departureTime = moment(obj.time, "YYYYMMDDHHmm");
+        if(!obj.hasOwnProperty('duration_in_traffic')) {
+          return {
+            departureTime: moment(departureTime).format("YYYY-MM-DD HH:mm"),
+            duration: obj.duration.value,
+            arriveTime: moment(departureTime).add(obj.duration.value, "seconds").format("YYYY-MM-DD HH:mm")
+          }
+        }
+        else {
+          return {
+            departureTime: moment(departureTime).format("YYYY-MM-DD HH:mm"),
+            duration: obj.duration_in_traffic.value,
+            arriveTime: moment(departureTime).add(obj.duration_in_traffic.value, "seconds").format("YYYY-MM-DD HH:mm")
+          }
+        }
+      });
+
+      console.log(displayData);
+      
+
+      
 
       // var demoData = [{
       //     label: 'somethingA',
       //     values: [{x: 'SomethingA', y: 10}, {x: 'SomethingB', y: 4}, {x: 'SomethingC', y: 3}]
       // }];
-      console.log(d3);
       var tooltip = function(x, y0, y, total) {
-        console.log('-----'+moment.duration(y, "seconds").humanize()+'-----');
+        // console.log('-----'+moment.duration(y, "seconds").humanize()+'-----');
     	  return moment.duration(y, "seconds").humanize();
     	};
       return(
-        <BarChart
-          data={chartData}
-          width={800}
-          height={400}
-          margin={{top: 10, bottom: 50, left: 50, right: 10}}
-          tooltipHtml={tooltip}
-          // color scale : http://synthesis.sbecker.net/articles/2012/07/16/learning-d3-part-6-scales-colors
-          // colorByLabel={false}
-          colorScale={ d3.scale.linear()
-            .domain([0, 800])
-            .range([0,400])
-          }
-          // fill={"teal"}
-          barPadding={0.4}
-        />
+        <div style={{width:'100%', height:'200px'}}>
+          <Info data={displayData} />
+          <div style={{}}>
+            <ResponsiveBarChart
+              data={chartData}
+              //width={800}
+              //height={400}
+              //margin={{top: 10, bottom: 50, left: 50, right: 10}}
+              tooltipHtml={tooltip}
+              // color scale : http://synthesis.sbecker.net/articles/2012/07/16/learning-d3-part-6-scales-colors
+              // colorByLabel={false}
+              //colorScale={ d3.scale.linear()
+              //  .domain([0, 800])
+              //  .range([0,400])
+              //}
+              // fill={"teal"}
+              barPadding={0.3}
+            />
+          </div>
+        </div>
       )
     }
   }
